9519fa8e8f925c663a53f8148deaee5e71858286
Harsh Rajmachikar <harshavardhan@rajmachikar.com>
2019-01-21 19:32:00 +0530

    Debugging

--- bunshi/home.py
+++ bunshi/home.py
@@ -8,41 +8,40 @@ recentMax = 5
 @app.route("/", methods = ["POST", "GET"])
 def home():
     flash(recent)
+
+    compound = ""
+    imageSource = ""
+    pageURL = ""
+
     try:
+        error = False
+
         if request.method == "POST":
             posts = request.form
-
             for post in posts.items():
                 compound = post[1].lower()
 
-            try:
-                error = False
-                links = getImage(compound)
-                imageSource = links[0]
-                pageURL = links[1]
+            links = getImage(compound)
+            imageSource = links[0]
+            pageURL = links[1]
 
-                if compound in recent:
-                    recent.remove(compound)
-                recent.insert(0, compound)
+            if compound in recent:
+                recent.remove(compound)
+            recent.insert(0, compound)
 
-                if len(recent) > recentMax:
-                    recent.pop(-1)
+            if len(recent) > recentMax:
+                recent.pop(-1)
 
-            return render_template("home.html",
-                                   imageSource = imageSource,
-                                   compound = compound,
-                                   pageURL = pageURL,
-                                   recent = recent)
+        return render_template("home.html",
+                               imageSource = imageSource,
+                               compound = compound,
+                               pageURL = pageURL,
+                               recent = recent)
 
     except TypeError as e:
         error = True
-        imageSource = ""
-        pageURL = ""
 
         return render_template("home.html",
                                compound = compound,
-                               error = esrror,
+                               error = error,
                                recent = recent)
-
-    # return render_template("home.html",
-    #                        recent = recent)
