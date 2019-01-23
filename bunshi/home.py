7ee552de7a1185cb487849421377e621292b214c
Harsh Rajmachikar <harshavardhan@rajmachikar.com>
2019-01-23 13:58:42 +0530

    Debugging

--- bunshi/home.py
+++ bunshi/home.py
@@ -2,12 +2,12 @@ from flask import flash, render_template, session, request
 from bunshi import app
 from bunshi.image import getImage
 
-recent = []
+session["recent"] = []
 recentMax = 5
 
 @app.route("/", methods = ["POST", "GET"])
 def home():
-    # flash(recent)
+    # flash(session["recent"])
     if request.method == "POST":
         session["compound"] = request.form["compound"].lower()
 
@@ -17,12 +17,12 @@ def home():
             imageSource = links[0]
             pageURL = links[1]
 
-            if session["compound"] in recent:
-                recent.remove(session["compound"])
-            recent.insert(0, session["compound"])
+            if session["compound"] in session["recent"]:
+                session["recent"].remove(session["compound"])
+            session["recent"].insert(0, session["compound"])
 
-            if len(recent) > recentMax:
-                recent.pop(-1)
+            if len(session["recent"]) > recentMax:
+                session["recent"].pop(-1)
 
         except TypeError as e:
             error = True
@@ -34,7 +34,7 @@ def home():
                                compound = session["compound"],
                                pageURL = pageURL,
                                error = error,
-                               recent = recent)
+                               recent = session["recent"])
 
     return render_template("home.html",
-                           recent = recent)
+                           recent = session["recent"])
